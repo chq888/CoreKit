68624a1e5de10f389268fe578446ef62673ac2bc
cuong.hoang <cuong.hoang@devsoft.vn>
2019-10-11 21:25:33 +0700

    add xamarin form

---
+++ CoreKit.XF/CoreKit.XF/CoreKit.XF/AppShell.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace CoreKit.XF
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+            //if (propertyName == nameof(CurrentState))
+            //{
+            //    string path = CurrentState.Location.OriginalString;
+            //    this.FlyoutBehavior = path.EndsWith("dashboard", System.StringComparison.Ordinal) || path.EndsWith("home", System.StringComparison.Ordinal) ? FlyoutBehavior.Flyout : FlyoutBehavior.Disabled;
+            //    Shell.SetFlyoutBehavior(this.CurrentItem, this.FlyoutBehavior);
+            //}
+        }
+    }
+}
---
+++ CoreKit.XF/CoreKit.XF/CoreKit.XF/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CoreKit.XF.Models;
+
+namespace CoreKit.XF.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
