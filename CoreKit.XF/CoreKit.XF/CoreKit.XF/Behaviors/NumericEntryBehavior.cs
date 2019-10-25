828fb82f71d42f9051ab220567bbe7a86b596272
cuong hoang <cuonghq612@gmail.com>
2019-10-25 18:14:33 +0700

    add behavior and trigger

---
+++ CoreKit.XF/CoreKit.XF/CoreKit.XF/Behaviors/NumericEntryBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CoreKit.XF.Behaviors
+{
+    public class NumericEntryBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.TextChanged += TextChanged_Handler;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= TextChanged_Handler;
+
+            base.OnDetachingFrom(bindable);
+        }
+
+        void TextChanged_Handler(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.NewTextValue))
+            {
+                return;
+            }
+
+            double currentValue;
+            if (!double.TryParse(e.NewTextValue, out currentValue))
+            {
+                ((Entry)sender).Text = e.OldTextValue;
+            }
+        }
+    }
+
+}
